ee1f03cd46dda400621bbce45e819d4ab2d23048
wzwleo <leo20050312@gmail.com>
2025-06-22 23:01:16 +0800

    Update gemini_api.py

--- gemini_api.py
+++ gemini_api.py
@@ -10,8 +10,7 @@ import os
 from dotenv import load_dotenv
 import google.generativeai as genai
 
-load_dotenv()
-api_key = os.getenv("GOOGLE_API_KEY")
+api_key = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
 
 def init_gemini_api():
